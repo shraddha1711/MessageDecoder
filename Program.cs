8052706a503af6a07f2965e4d968fec6ec6c873a
Shraddha Shukla <shraddhashukla327@gmail.com>
2026-01-08 13:07:45 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+      public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter The name ");
+        string name = Console.ReadLine();
+       
+       int VowelCount = name.ToLower().Count(c=> "aeiou".Contains(c));
+       Console.WriteLine(VowelCount);
+    //    foreach(char c in name.ToLower())
+    //     {
+    //         if(c=='a' || c=='e' || c=='i' ||c == 'o' || c == 'u')
+    //         {
+    //             VowelCount++;
+
+    //         }
+    //     }
+    // foreach(char c in name.ToLower())
+    //     {
+    //         if ("aeiou".Contains(c))
+    //         {
+    //             VowelCount++;
+    //             Console.WriteLine($"Found Vowel : {c} | Total So far{VowelCount}");
+    //         } 
+    //     }
+    //    Console.WriteLine($"Vowel in this name : {VowelCount} ");
+        
+    // }
+    }
+
+}
