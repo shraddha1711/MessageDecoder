51e75f611b29bfba37fb874e96e10b43a899b93a
Shraddha Shukla <shraddhashukla327@gmail.com>
2026-01-08 13:27:27 +0530

    Updated Program

--- Program.cs
+++ Program.cs
@@ -3,11 +3,11 @@ class Program
 {
       public static void Main(string[] args)
     {
-        Console.WriteLine("Enter The name ");
-        string name = Console.ReadLine();
+    //     Console.WriteLine("Enter The name ");
+    //     string name = Console.ReadLine();
        
-       int VowelCount = name.ToLower().Count(c=> "aeiou".Contains(c));
-       Console.WriteLine(VowelCount);
+    //    int VowelCount = name.ToLower().Count(c=> "aeiou".Contains(c));
+    //    Console.WriteLine(VowelCount);
     //    foreach(char c in name.ToLower())
     //     {
     //         if(c=='a' || c=='e' || c=='i' ||c == 'o' || c == 'u')
@@ -27,6 +27,26 @@ class Program
     //    Console.WriteLine($"Vowel in this name : {VowelCount} ");
         
     // }
+
+       Console.WriteLine("Enter THe Size");
+       int n = Convert.ToInt32(Console.ReadLine());
+       int[] trasures = new int[n];
+       for(int i = 0; i < n; i++)
+        {
+            trasures[i] = Convert.ToInt32(Console.ReadLine());
+        }
+    //    int max = trasures[0]; 
+
+    //     for(int i = 0; i < n; i++)
+    //     {
+    //         if(trasures[i] > max)
+    //         {
+    //             max = trasures[i];
+    //         }
+    //     }
+    int max = trasures.Max();
+        Console.WriteLine(max);
+
     }
 
 }
